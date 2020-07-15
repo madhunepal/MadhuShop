cf8a8ecfd8756309d0d866dc272f64df61a6e129
madhunepal <madhunepal@gmail.com>
2020-07-14 22:34:23 -0400

    added model and created database

---
+++ DataLayer/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using MadhuShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MadhuShop.DataLayer
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Catogory { get; set; }
+    }
+}
